0e6e2df362bed5b360cdecf93bbc338b7311f90b
noah-10 <noah.fryman.10@gmail.com>
2024-02-27 14:46:17 -0500

    Added a user route for creating profile, logging in and logging out

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -6,7 +6,6 @@ const { User, Blog } = require('../models');
 
 
 //Get all blogs with users
-
 router.get('/', async (req, res) => {
     try{
         const allBlogs = await Blog.findAll({
@@ -22,8 +21,57 @@ router.get('/', async (req, res) => {
         });
 
     }catch (err){
-        res.json("Error with getting all projects", err);
+        res.json("Error with getting all blogs", err);
     }
+});
+
+// route for a single blog
+router.get('/blog/:id', async (req, res) => {
+    try{
+        const blogData = await Blog.findByPk(req.params.id, {
+            include: {model: User}
+        })
+
+        const blog = blogData.get({ plain: true });
+
+        res.render('blog', {
+            ...blog,
+            logged_in: req.session.logged_in
+        });
+    }catch(err){
+        res.json("Error with getting a single blog", err);
+    }
+});
+
+//route for getting profile
+// ADD AUTH AND AN ATTRIBTE FOR EXCLUDING PASSWORD
+router.get('/profile', async (req, res) => {
+    try{
+        const userData = await User.findByPk(req.session.user_id, {
+            include: {model: Blog}
+        });
+
+        const user = userData.get({ plain: true });
+
+        res.render('profile', {
+            ...user,
+            logged_in: true
+        });
+    }catch(err){
+        res.json("Error with loading profile", err);
+    }
+});
+
+// route for login
+router.get('/login', async (req, res) => {
+
+    //If user is already logged in, then it will redirect them
+    if(req.session.logged_in){
+        res.redirect('/profile')
+        return;
+    }
+
+    res.render('login');
 })
 
 module.exports = router; 
