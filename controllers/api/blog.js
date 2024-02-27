17f5dfa2615e3f3b3ecc50c22ffd698856874975
noah-10 <noah.fryman.10@gmail.com>
2024-02-27 15:02:19 -0500

    Added a blog route for creating, updating and deleting blogs

--- controllers/api/blog.js
+++ controllers/api/blog.js
@@ -1,1 +1,60 @@
 // Will contain post, put, delete for blogs
+const router = require('express').Router();
+const { Blog } = require('../../models/Blog');
+const withAuth = require('../../utils/withAuth');
+
+//create new blog
+router.post('/', withAuth, async (req, res) => {
+    try{
+        const newBlog = await Blog.create({
+            ...req.body,
+            user_id: req.session.user_id
+        });
+
+        res.json(newBlog);
+    }catch(err){
+        res.json({message: "Error with creating new blog"});
+    }
+});
+
+//Updating blogs
+router.put('/:id', withAuth, async (req, res) => {
+    try{
+        const blogData = await Blog.update(req.body, {
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id
+            },
+        })
+
+        if(!blogData){
+            res.json({message: "No blog found with this id!"});
+            return;
+        }
+        
+        res.json(blogData);
+    }catch(err){
+        res.json({message: "Error with updating blog"});
+    }
+});
+
+//Deleting blogs
+router.delete('/:id', withAuth, async (req, res) => {
+    try{
+        const deleteBlog = await Blog.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id
+            },
+        });
+
+        if(!deleteBlog){
+            res.json({message: "No blog found with this id!"})
+            return;
+        }
+
+        res.json(deleteBlog);
+    }catch(err){
+        res.json({message: "Error with deleting blog", err});
+    }
+})
--- controllers/api/user.js
+++ controllers/api/user.js
@@ -57,3 +57,5 @@ router.post('/logout', async (req,res) => {
         res.status(404).end();
     }
 });
+
+module.exports = router;
