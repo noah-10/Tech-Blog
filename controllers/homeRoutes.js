19581c03d5626810a0d20fd4f658eb79126daf45
noah-10 <noah.fryman.10@gmail.com>
2024-03-01 15:00:57 -0500

    Added html to dashboard page with some css

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,89 +1,83 @@
-const router = require('express').Router();
-const { User, Blog, Comment } = require('../models');
+const router = require("express").Router();
+const { User, Blog, Comment } = require("../models");
 const withAuth = require("../utils/withAuth");
 // Require path files
 
 // Fetch for homepage
 
-
 //Get all blogs with users
-router.get('/', async (req, res) => {
-    try{
-        const allBlogs = await Blog.findAll({
-            include: {model: User}
-        });
+router.get("/", async (req, res) => {
+  try {
+    const allBlogs = await Blog.findAll({
+      include: { model: User },
+    });
 
-        //serialize data so template can read the data better
-        const blogs = allBlogs.map(blog => blog.get({ plain: true }));
+    //serialize data so template can read the data better
+    const blogs = allBlogs.map((blog) => blog.get({ plain: true }));
 
-        res.render('homepage', {
-            blogs,
-            logged_in: req.session.logged_in
-        });
-
-    }catch (err){
-        res.json("Error with getting all blogs", err);
-    }
+    res.render("homepage", {
+      blogs,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.json("Error with getting all blogs", err);
+  }
 });
 
 // route for a single blog
-router.get('/blog/:id', async (req, res) => {
-    try{
-        const blogData = await Blog.findByPk(req.params.id, {
-            include: [
-                {model: User},
-                {model: Comment, include: [User]}
-            ],
-        })
+router.get("/blog/:id", async (req, res) => {
+  try {
+    const blogData = await Blog.findByPk(req.params.id, {
+      include: [{ model: User }, { model: Comment, include: [User] }],
+    });
 
-        if(!blogData){
-            res.json({message: "No blog found with this id!"});
-        }
-
-        const blog = blogData.get({ plain: true });
-
-        res.render('blog', {
-            blog,
-            logged_in: req.session.logged_in
-        });
-    }catch(err){
-        res.json("Error with getting a single blog", err);
+    if (!blogData) {
+      res.json({ message: "No blog found with this id!" });
     }
+
+    const blog = blogData.get({ plain: true });
+
+    res.render("blog", {
+      blog,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.json("Error with getting a single blog", err);
+  }
 });
 
 //route for getting profile
 // ADD AUTH AND AN ATTRIBTE FOR EXCLUDING PASSWORD
-router.get('/dashboard', withAuth, async (req, res) => {
-    try{
-        const userData = await User.findByPk(req.session.user_id, {
-            include: {model: Blog}
-        });
+router.get("/dashboard", withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      include: { model: Blog },
+    });
 
-        const user = userData.get({ plain: true });
+    const user = userData.get({ plain: true });
 
-        res.render('dashboard', {
-            ...user,
-            logged_in: true
-        });
-    }catch(err){
-        res.json("Error with loading profile", err);
-    }
+    res.render("dashboard", {
+      user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.json("Error with loading profile", err);
+  }
 });
 
 // route for login
-router.get('/login', async (req, res) => {
+router.get("/login", async (req, res) => {
+  //If user is already logged in, then it will redirect them
+  if (req.session.logged_in) {
+    res.redirect("/dashboard");
+    return;
+  }
 
-    //If user is already logged in, then it will redirect them
-    if(req.session.logged_in){
-        res.redirect('/dashboard')
-        return;
-    }
-
-    res.render('login');
-})
-
-router.get('/sign-up', async (req, res) => {
-    res.render('sign-up')
+  res.render("login");
 });
 
-module.exports = router; 
+router.get("/sign-up", async (req, res) => {
+  res.render("sign-up");
+});
+
+module.exports = router;
